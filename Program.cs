1f3761fb8c46505a09f8465d5a1b6e0d49319766
BhagyashriSabale <balisabale16@gmail.com>
2023-05-14 22:09:02 +0530

    [Bhagyashri] Find maximun number in floats values

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@
             int[] intArray = { 100, 200, 300 };
             MaximumNum<int> maximum = new MaximumNum<int>(intArray);
             maximum.PrintMaxVlue();
+
+            double[] doubleArray = { 435.3, 12.4, 187.45 };
+            MaximumNum<double> maximum1 = new MaximumNum<double>(doubleArray);
+            maximum1.PrintMaxVlue();
         }
     }
 }
