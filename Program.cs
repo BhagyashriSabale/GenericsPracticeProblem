8c4081aafa38bef7b368bd46a7f1ff3fa9e78d15
BhagyashriSabale <balisabale16@gmail.com>
2023-05-14 22:02:30 +0530

    [Bhagyashri] Find Maximun Number in integer values

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome Find Maximum numer Problem");
+
+            int[] intArray = { 100, 200, 300 };
+            MaximumNum<int> maximum = new MaximumNum<int>(intArray);
+            maximum.PrintMaxVlue();
         }
     }
 }
