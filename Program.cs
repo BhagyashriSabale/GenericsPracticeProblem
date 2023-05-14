6ee0b65cb473832a7494e0b8ac5de4ec02234ec8
BhagyashriSabale <balisabale16@gmail.com>
2023-05-14 22:14:46 +0530

    [Bhagyashri] Find maximum string in Different strings

--- Program.cs
+++ Program.cs
@@ -13,6 +13,10 @@
             double[] doubleArray = { 435.3, 12.4, 187.45 };
             MaximumNum<double> maximum1 = new MaximumNum<double>(doubleArray);
             maximum1.PrintMaxVlue();
+
+            string[] strings = { "Red", "Black", "Orange" };
+            MaximumNum<string> maximum2 = new MaximumNum<string>(strings);
+            maximum2.PrintMaxVlue();
         }
     }
 }
