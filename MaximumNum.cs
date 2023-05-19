45cbb9e0e9c4ec7eb6c4676a7073883ba300c093
BhagyashriSabale <balisabale16@gmail.com>
2023-05-20 01:57:25 +0530

    [Bhagyashri] All 3 use cases in one generic method

--- MaximumNum.cs
+++ MaximumNum.cs
@@ -9,30 +9,30 @@ namespace GenericsPracticeProblem
     internal class MaximumNum<T> where T : IComparable
     {
         public T[] value;
-        public MaximumNum(T[] value) 
+        public MaximumNum(T[] value)
         {
             this.value = value;
         }
-        public T[] Sort(T[] values)
+        public T FindMaximum()
         {
-            Array.Sort(values);
-            return values;
-        }
-        public T MaxValue(params T[] values)
-        {
-            var sorted_values = Sort(values);
-            return sorted_values[^1];
-        }
-        public T MaxMethod()
-        {
-            var max = MaxValue(this.value);
-            return max;
+            T maximum = value[^1]; // Assume the first element is the maximum
 
+            for (int i = 1; i < value.Length; i++)
+            {
+                if (value[i].CompareTo(maximum) > 0)
+                {
+                    maximum = value[i];
+                }
+            }
+
+            return maximum;
         }
-        public void PrintMaxVlue()
+        public void PrintMaximumValue()
         {
-            var max = MaxValue(this.value);
-            Console.WriteLine("Maximum value is " + max);
+            T maximum = FindMaximum();
+            Console.WriteLine("Maximum value is " + maximum);
         }
+        
     }
+
 }
--- Program.cs
+++ Program.cs
@@ -8,15 +8,15 @@
 
             int[] intArray = { 100, 200, 300 };
             MaximumNum<int> maximum = new MaximumNum<int>(intArray);
-            maximum.PrintMaxVlue();
+            maximum.PrintMaximumValue();
 
             double[] doubleArray = { 435.3, 12.4, 187.45 };
             MaximumNum<double> maximum1 = new MaximumNum<double>(doubleArray);
-            maximum1.PrintMaxVlue();
+            maximum1.PrintMaximumValue();
 
             string[] strings = { "Red", "Black", "Orange" };
             MaximumNum<string> maximum2 = new MaximumNum<string>(strings);
-            maximum2.PrintMaxVlue();
+            maximum2.PrintMaximumValue();
         }
     }
 }
